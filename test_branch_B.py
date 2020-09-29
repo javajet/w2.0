5c5a3e61c71adf09653c733fde7dc7ce2f99607d
David John George <javajet@hotmail.com>
2020-09-29 17:53:35 +0100

    Testing git branching

---
+++ test_branch_B.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 
+# MAGIC # First Comment
+
+# COMMAND ----------
+
+# MAGIC %md # Second comment (only on branch user_A)
+
+# COMMAND ----------
+
